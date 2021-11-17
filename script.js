c8900dea62d5a0b729841b9afcc931d80dd90180
Sasha O'Donovan <sasha.odonovan@gmail.com>
2021-11-17 13:54:52 +0000

    ships now pushed to array, with handleShoot function

--- script.js
+++ script.js
@@ -1,124 +1,93 @@
+const shootBTN = document.querySelector(".game__shootBTN");
+const restartBTN = document.querySelector(".game__restartBTN");
 
+const shipsArray = [];
 
-// ** click the button 
-// ** it finds a random face/ship in the game - via an array of the faces/ships
-// *alert the ship that's going to be deducted 
+class Ships {
+  constructor(type, hitPoints, health) {
+    this.type = type;
+    this.hitPoints = hitPoints;
+    this.health = health;
+  }
 
+  createMotherShips(amt) {
+    for (let i = 0; i < amt; i++) {
+      shipsArray.push(motherShips);
+    }
+  }
 
-const shipsArray = [ "mothership", "defenceship", "attackship"];
+  createDefenceShips(amt) {
+    for (let i = 0; i < amt; i++) {
+      shipsArray.push(defenceShips);
+    }
+  }
 
-const selectRandomShip = () => {
-const randomShip = shipsArray[Math.floor(Math.random()*shipsArray.length)];
-alert (randomShip)
+  createAttackShips(amt) {
+    for (let i = 0; i < amt; i++) {
+      shipsArray.push(attackShips);
+    }
+  }
 }
 
+const motherShips = new Ships("motherShip", 9, 100);
+motherShips.createMotherShips(1);
+// create defence ships using class method
+const defenceShips = new Ships("defenceShip", 10, 80);
+defenceShips.createDefenceShips(6);
 
-const shootBTN = document.querySelector(".game__shootBTN");
+// create attackships using class method
 
+const attackShips = new Ships("attackShip", 12, 45);
+attackShips.createAttackShips(8);
 
+console.log(shipsArray); // your goal.. is the  get an array, with ship objects, and those ship objects need to come from a class
 
-shootBTN.addEventListener("click", selectRandomShip)
+const handleShoot = () => {
+  // taking the shipsArray and using the math method to randomly select an item/index in the array
+  const randomShip = shipsArray[Math.floor(Math.random() * shipsArray.length)];
 
+  // deduct points from ship
+  // everytime it clicks, we need to deduct hitPoints
+  const nameOfShip = randomShip.type;
+  const totalHealth = randomShip.health;
+  const hitPoints = randomShip.hitPoints;
+  const deductPointsFromHealth = totalHealth - hitPoints;
 
+  randomShip.health = deductPointsFromHealth;
 
-// ** create object/class of the ships - connected to the dom?
-// **loop map through the faces in the dom ? 
-// ** create array 
+  console.log(`The ${nameOfShip} is now ${deductPointsFromHealth}`);
 
+  console.log(deductPointsFromHealth);
 
+  // if mothership health = 0 then WIN + winning message
+  const winningPopUp = document.querySelector(".winning__message");
 
+  const winningMessage = () => {
+    if (motherShips.health <= 1) {
+      winningPopUp.innerHTML = `
+    <div>
+      <h1>YOU WIN! YOU TURNED THAT FROWN UPSIDE DOWN!
+      </h1>
+      <i id="happyface__grin" class="fas fa-grin-stars fa-7x "></i>
+    </div>`;
+    } else if (
+      defenceShips.health <= 0 &&
+      attackShips.health <= 0 &&
+      motherShips.health <= 0
+    ) {
+      winningPopUp.innerHTML = `
+    <div>
+      <h1>YOU WIN! YOU TURNED THAT FROWN UPSIDE DOWN!
+      </h1>
+      <i id="happyface__grin" class="fas fa-grin-stars fa-7x "></i>
+    </div>`;
+    }
+    return winningMessage;
+  };
 
+  winningMessage();
+};
 
+shootBTN.addEventListener("click", handleShoot);
 
-
-
-
-
-
-
-
-
-
-
-
-
-// create a class for the ships
-// create the ships
-// create a method to deduct damage points from totalHitPoints 
-//  create array of the ships 
-// create a variable that randomly chooses an index item from the array 
-// deduct the hitpoints from random index item selected 
-// update the score
-
-// once it reaches certain hitPoints change faces 
-
-// gamover if you reach 100
-
-
-// class gameShips {
-//   constructor (type, totalHitPoints, damagePoints ){
-//   this.type = type;
-//   this. totalHitPoints = totalHitPoints;
-//   this.damagePoints = damagePoints;
-//   }
-// // function to deduct damage from total hit points 
-//   takeDamage() {
-//     this.totalHitPoints = this.totalHitPoints - this.damagePoints;
-//   }
-
-//   // function to create defence smiles and add to array 
-
-//   createDefenceSmileys (amt) {
-//     for(let i=0; i < amt; i++) {
-
-//     const defenceSmiley = new defenceSmiley.ships.push(defenceSmiley)
-//     console.log(createDefenceSmileys(10));
-//   }
-// }
-
-// // functio to create attack smiles and add to array 
-
-//   createAttackSmileys(amt) {
-//     for(let i=0; i < amt; i++) {
-
-//       const attackSmiley = new attackSmiley.ships.push(attackSmiley)
-//       console.log(createAttackSmileys(10));
-//   }
-// }
-// // function to create mothership and add to array
-//     createMothership (amt) {
-//     const motherShipSmiley = new motherShipSmiley.ships.push(attackSmiley)
-//     console.log(createAttackSmileys(10));
-// }
-
-//   // variable for the array 
-//   const ships = []
-//   console.log(ships);
-
-// // create the array 
-
-//     smileyArray ()  {
-//       motherShipSmiley.ships.push()
-//       attackSmiley.ships.push()
-//       defenceSmiley.ships.push()    
-//     }
-
-// // choose random item in the array 
-
-// const randomSmiley = ships.[Math.floor(Math.random()*ships.length)];
-
-// // deduct hitPoints from random item in the array 
-
-//     deductHitPoints () {
-//       randomSmiley - this.damagePoints;
-//     }
-
-//     // deduct from UI score 
-
-//     deduct
-
-
-// }
-
-
-
+// restartBTN.addEventListener("click", location.reload())
