0a17fe9fefb52c9677e86ec5548e1b4de5ecb82e
Sasha O'Donovan <sasha.odonovan@gmail.com>
2021-11-17 16:00:10 +0000

    working array and win conditions

--- script.js
+++ script.js
@@ -10,37 +10,36 @@ class Ships {
     this.health = health;
   }
 
-  createMotherShips(amt) {
+  static createMotherShips(amt) {
     for (let i = 0; i < amt; i++) {
+      const motherShips = new Ships("motherShip", 9, 100);
       shipsArray.push(motherShips);
     }
   }
 
-  createDefenceShips(amt) {
+  // static method, allows you to call the method on the class itself
+  static createDefenceShips(amt) {
     for (let i = 0; i < amt; i++) {
+      const defenceShips = new Ships("defenceShip", 10, 80);
       shipsArray.push(defenceShips);
     }
   }
 
-  createAttackShips(amt) {
+  static createAttackShips(amt) {
     for (let i = 0; i < amt; i++) {
+      const attackShips = new Ships("attackShip", 12, 45);
       shipsArray.push(attackShips);
     }
   }
 }
 
-const motherShips = new Ships("motherShip", 9, 100);
-motherShips.createMotherShips(1);
-// create defence ships using class method
-const defenceShips = new Ships("defenceShip", 10, 80);
-defenceShips.createDefenceShips(6);
+Ships.createMotherShips(1);
 
-// create attackships using class method
+Ships.createDefenceShips(6);
 
-const attackShips = new Ships("attackShip", 12, 45);
-attackShips.createAttackShips(8);
+Ships.createAttackShips(8);
 
-console.log(shipsArray); // your goal.. is the  get an array, with ship objects, and those ship objects need to come from a class
+// your goal.. is the  get an array, with ship objects, and those ship objects need to come from a class
 
 const handleShoot = () => {
   // taking the shipsArray and using the math method to randomly select an item/index in the array
@@ -62,32 +61,46 @@ const handleShoot = () => {
   // if mothership health = 0 then WIN + winning message
   const winningPopUp = document.querySelector(".winning__message");
 
-  const winningMessage = () => {
-    if (motherShips.health <= 1) {
+  const index = shipsArray.indexOf(randomShip);
+  if (randomShip.health <= 0) {
+    shipsArray.splice(index, 1);
+  }
+
+  const noMoreArrayWin = () => {
+    if (shipsArray.length < 0) {
       winningPopUp.innerHTML = `
-    <div>
-      <h1>YOU WIN! YOU TURNED THAT FROWN UPSIDE DOWN!
-      </h1>
-      <i id="happyface__grin" class="fas fa-grin-stars fa-7x "></i>
-    </div>`;
-    } else if (
-      defenceShips.health <= 0 &&
-      attackShips.health <= 0 &&
-      motherShips.health <= 0
-    ) {
-      winningPopUp.innerHTML = `
-    <div>
-      <h1>YOU WIN! YOU TURNED THAT FROWN UPSIDE DOWN!
-      </h1>
-      <i id="happyface__grin" class="fas fa-grin-stars fa-7x "></i>
-    </div>`;
+      <div>
+        <h1>YOU WIN! YOU TURNED THAT FROWN UPSIDE DOWN!
+        </h1>
+        <i id="happyface__grin" class="fas fa-grin-stars fa-7x "></i>
+      </div>`;
+      console.log("no more array win");
+      return noMoreArrayWin;
     }
-    return winningMessage;
   };
 
-  winningMessage();
+  shipsArray.forEach(function (i) {
+    if (i.type === "motherShip" && i.health <= 1) {
+      winningPopUp.innerHTML = `
+        <div>
+          <h1>YOU WIN! YOU TURNED THAT FROWN UPSIDE DOWN!
+          </h1>
+          <i id="happyface__grin" class="fas fa-grin-stars fa-7x "></i>
+        </div>`;
+    }
+  });
+
+  console.log(shipsArray);
 };
 
 shootBTN.addEventListener("click", handleShoot);
 
 // restartBTN.addEventListener("click", location.reload())
+
+// loop through the shipsArray and check each ships health
+
+// conditions
+// if currentindex.name = mothership and health = 0
+//
+// if 0 remove from array
+// if array.length = 0 also gameover
