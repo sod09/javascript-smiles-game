5ee38deaa625a134fb7e8c0a378023bb15fd2b3d
Sasha O'Donovan <sasha.odonovan@gmail.com>
2021-11-23 13:15:59 +0000

    read me updated

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const shootBTN = document.querySelector(".game__shootBTN");
 const restartBTN = document.querySelector(".game__restartBTN");
-const gridContainer = document.querySelector(".game__sadfaces")
+const gridContainer = document.querySelector(".game__sadfaces");
+const winningPopUp = document.querySelector(".winning__message");
 
+// an array to contain the ships in the game
 const shipsArray = [];
 
+// taking an OOP approach, create a class for the ships and then methods to create the ships themselves
 class Ships {
   constructor(type, hitPoints, health) {
     this.type = type;
@@ -23,7 +26,6 @@ class Ships {
     for (let i = 0; i < amt; i++) {
       const defenceShips = new Ships("defenceShip", 10, 80);
       shipsArray.push(defenceShips);
-
     }
   }
 
@@ -34,123 +36,141 @@ class Ships {
     }
   }
 
-  static createHTMLShips () {
+  // this method creates the HTML ships in the game, it loops through and for every ship appends the correct div and classes
+  static createHTMLShips() {
     for (let i = 0; i < shipsArray.length; i++) {
       if (shipsArray[i].type === "motherShip") {
-       const createShipDiv = document.createElement("div");
-       createShipDiv.classList.add("sadface__angrycontainer");
-       const createiTag = document.createElement("i");
-       createiTag.classList.add("sadface__angry", "sad__faces", "fas", "fa-angry", "fa-4x");
-       createShipDiv.appendChild(createiTag);
-       const createHitPointsDiv = document.createElement("div");
-       createHitPointsDiv.classList.add("sadface__angryhitpoints");
-       createHitPointsDiv.innerHTML = `${shipsArray[i].health}`
-       createShipDiv.appendChild(createHitPointsDiv);
-       gridContainer.appendChild(createShipDiv);
-       
-        
-       if (shipsArray[i].health <= 1) {
-        shipsArray[i].health = "";
-        createiTag.classList.remove("sadface__angry", "sad__faces", "fas", "fa-angry", "fa-4x");
-        createiTag.classList.add("fas", "fa-grin-stars", "fa-4x");
-        createiTag.style.color= "#FF2DA0"
-      }
+        const createShipDiv = document.createElement("div");
+        createShipDiv.classList.add("sadface__angrycontainer");
+        const createiTag = document.createElement("i");
+        createiTag.classList.add(
+          "sadface__angry",
+          "sad__faces",
+          "fas",
+          "fa-angry",
+          "fa-4x"
+        );
+        createShipDiv.appendChild(createiTag);
+        const createHitPointsDiv = document.createElement("div");
+        createHitPointsDiv.classList.add("sadface__angryhitpoints");
+        createHitPointsDiv.innerHTML = `${shipsArray[i].health}`;
+        createShipDiv.appendChild(createHitPointsDiv);
+        gridContainer.appendChild(createShipDiv);
+
+        if (shipsArray[i].health <= 1) {
+          shipsArray[i].health = "";
+          createiTag.classList.remove(
+            "sadface__angry",
+            "sad__faces",
+            "fas",
+            "fa-angry",
+            "fa-4x"
+          );
+          createiTag.classList.add("fas", "fa-grin-stars", "fa-4x");
+          createiTag.style.color = "#FF2DA0";
+        }
       }
     }
 
     for (let i = 0; i < shipsArray.length; i++) {
       if (shipsArray[i].type === "defenceShip") {
-       const createShipDiv = document.createElement("div");
-       createShipDiv.classList.add(`sadface__crycontainer${[i]}`);
-    
-       const createiTag = document.createElement("i");
-       createiTag.classList.add("sad__facescry", "sad__faces", "fas", "fa-sad-cry", "fa-4x");
-       createShipDiv.appendChild(createiTag);
-       const createHitPointsDiv = document.createElement("div");
-       createHitPointsDiv.classList.add(`sadface__cryhitpoints${[i]}`);
-       createHitPointsDiv.innerHTML = `${shipsArray[i].health}`
-       createShipDiv.appendChild(createHitPointsDiv);
-       gridContainer.appendChild(createShipDiv);
+        const createShipDiv = document.createElement("div");
+        createShipDiv.classList.add(`sadface__crycontainer${[i]}`);
 
-        
+        const createiTag = document.createElement("i");
+        createiTag.classList.add(
+          "sad__facescry",
+          "sad__faces",
+          "fas",
+          "fa-sad-cry",
+          "fa-4x"
+        );
+        createShipDiv.appendChild(createiTag);
+        const createHitPointsDiv = document.createElement("div");
+        createHitPointsDiv.classList.add(`sadface__cryhitpoints${[i]}`);
+        createHitPointsDiv.innerHTML = `${shipsArray[i].health}`;
+        createShipDiv.appendChild(createHitPointsDiv);
+        gridContainer.appendChild(createShipDiv);
+
         if (shipsArray[i].health < 1) {
           shipsArray[i].health = "";
-          createiTag.classList.remove("sad__facescry", "sad__faces", "fas", "fa-sad-cry", "fa-4x");
+          createiTag.classList.remove(
+            "sad__facescry",
+            "sad__faces",
+            "fas",
+            "fa-sad-cry",
+            "fa-4x"
+          );
           createiTag.classList.add("fas", "fa-smile-wink", "fa-4x");
-          createiTag.style.color= "#FF2DA0"
+          createiTag.style.color = "#FF2DA0";
         }
-       
       }
-
     }
-    
-    
+
     for (let i = 0; i < shipsArray.length; i++) {
       if (shipsArray[i].type === "attackShip") {
-       const createShipDiv = document.createElement("div");
-       createShipDiv.classList.add(`sadface__tearcontainer${[i]}`);
-    
-       const createiTag = document.createElement("i");
-       createiTag.classList.add("sad__facetears", "sad__faces", "fas", "fa-sad-tear", "fa-4x");
-       createShipDiv.appendChild(createiTag);
-       const createHitPointsDiv = document.createElement("div");
-       createHitPointsDiv.classList.add(`sadface__tearhitpoints${[i]}`);
-       createHitPointsDiv.innerHTML = `${shipsArray[i].health}`
-       createShipDiv.appendChild(createHitPointsDiv);
-       gridContainer.appendChild(createShipDiv);
+        const createShipDiv = document.createElement("div");
+        createShipDiv.classList.add(`sadface__tearcontainer${[i]}`);
 
-       if (shipsArray[i].health < 1) {
-        shipsArray[i].health = "";
-        createiTag.classList.remove("sad__facetears", "sad__faces", "fas", "fa-sad-tear", "fa-4x");
-        createiTag.classList.add("fas", "fa-smile", "fa-4x");
-        createiTag.style.color= "#FF2DA0"
-      }
-       
-      }
+        const createiTag = document.createElement("i");
+        createiTag.classList.add(
+          "sad__facetears",
+          "sad__faces",
+          "fas",
+          "fa-sad-tear",
+          "fa-4x"
+        );
+        createShipDiv.appendChild(createiTag);
+        const createHitPointsDiv = document.createElement("div");
+        createHitPointsDiv.classList.add(`sadface__tearhitpoints${[i]}`);
+        createHitPointsDiv.innerHTML = `${shipsArray[i].health}`;
+        createShipDiv.appendChild(createHitPointsDiv);
+        gridContainer.appendChild(createShipDiv);
 
+        if (shipsArray[i].health < 1) {
+          shipsArray[i].health = "";
+          createiTag.classList.remove(
+            "sad__facetears",
+            "sad__faces",
+            "fas",
+            "fa-sad-tear",
+            "fa-4x"
+          );
+          createiTag.classList.add("fas", "fa-smile", "fa-4x");
+          createiTag.style.color = "#FF2DA0";
+        }
+      }
     }
   }
 }
-
 
 // call the method within ships to create ships and push to an array
 Ships.createMotherShips(1);
-
 Ships.createDefenceShips(6);
-
 Ships.createAttackShips(8);
-
-Ships.createHTMLShips()
+Ships.createHTMLShips();
 
 let randomShip;
 
-
-// programming method called recursion, like a for loop in a function - function runs itself until it gives us the desired outcome  
+// programming method called recursion, like a for loop in a function - function runs itself until it gives us the desired outcome
 const getRandomShip = () => {
-
- randomShip= shipsArray[Math.floor(Math.random() * shipsArray.length)];
-
+  randomShip = shipsArray[Math.floor(Math.random() * shipsArray.length)];
   if (randomShip.health < 1) {
-    getRandomShip()
+    getRandomShip();
   }
-}
+};
 
 const handleShoot = () => {
   // taking the shipsArray and using the math method to randomly select an item/index in the array
-  // const randomShip = shipsArray[Math.floor(Math.random() * shipsArray.length)];
   getRandomShip();
 
   // everytime it clicks, we need to deduct hitPoints
-  const nameOfShip = randomShip.type;
   const totalHealth = randomShip.health;
   const hitPoints = randomShip.hitPoints;
   const deductPointsFromHealth = totalHealth - hitPoints;
-
   randomShip.health = deductPointsFromHealth;
 
   // if mothership health = 0 then WIN + winning message
-  const winningPopUp = document.querySelector(".winning__message");
-
   shipsArray.forEach(function (i) {
     if (i.type === "motherShip" && i.health <= 1) {
       winningPopUp.innerHTML = `
@@ -160,14 +180,11 @@ const handleShoot = () => {
           <i id="happyface__grin" class="fas fa-grin-stars fa-7x"></i>
         </div>`;
     }
-  
   });
 
-  gridContainer.innerHTML = ""
-
-  Ships.createHTMLShips()
-
-}
+  gridContainer.innerHTML = "";
+  Ships.createHTMLShips();
+};
 
 shootBTN.addEventListener("click", handleShoot);
 
